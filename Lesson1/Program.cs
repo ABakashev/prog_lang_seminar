2ab1f2bf209206c26e6646ca514be3c8a7156254
Adam <adam.bakashev.r@gmail.com>
2025-07-30 15:02:31 +0500

    Solved 2 cases

---
+++ Lesson1/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Это команда выводит текст
+Console.WriteLine("Hello World!");
+Console.WriteLine("Lets check if 1st number is square of 2nd number.");
+Console.WriteLine("Please, write 1st number.");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please, write 2nd number.");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2 * num2)
+{
+    Console.WriteLine("It is true");
+}
+else
+{
+    Console.WriteLine("It is false");
+}*/
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int negNum = -num;
+
+while (negNum <= num)
+{
+    Console.Write(negNum + " ");
+    negNum++;
+}
